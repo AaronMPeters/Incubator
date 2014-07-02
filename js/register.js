defa0755ec06bad82515e8051605ea23b267ea04
AaronMPeters <peter177@purdue.edu>
2014-07-02 15:31:03 -0400

    Finished registration page

--- js/register.js
+++ js/register.js
@@ -4,11 +4,16 @@ window.onload = function(){
 	var name_split = name.split(' ');
 
 	
-	document.getElementById("welcome").innerHTML = "Alright "+ name_split[0] + ", let's get some information about you";
+	document.getElementById("welcome").innerHTML = "Alright "+ name_split[0] + ", let's get some basic information about you";
 	var form = document.forms[0];
 	form.elements["name"].value = name;
 	form.elements["email"].value = email.substring(0, email.lastIndexOf("."));
 
 	var emailEnding = email.substring(email.lastIndexOf(".") + 1, email.length);
-	document.getElementById(emailEnding).selected = true;
+	if (emailEnding == "edu" || emailEnding == "com" || emailEnding == "org" || emailEnding == "net")
+		document.getElementById(emailEnding).selected = true;
+	else{
+		document.getElementById("other").selected = true;
+		document.getElementById("other").innerHTML = "." + emailEnding;
+	}
 }
